a85358d3499d6862d9b8159eaf3884182b96db10
MalcolmSmalls <smallsbeats@gmail.com>
2022-09-28 14:26:45 -0700

    Add project to array when created

--- src/createProject.js
+++ src/createProject.js
@@ -1,12 +1,25 @@
+const projectList = []
 const createProject = (() => {
     const createProj = document.querySelector('#project')
     const projectArea = document.querySelector('.projectSelected')
     const projectName = () => {
         if(createProj.value="newProject"){
-            projectArea.innerHTML = `<label for="projectName">Project Name:</label> <input type="text" name="projectName" id="projectName">`
+            projectArea.innerHTML = `<label for="projectName">Project Name:</label> <input type="text" name="projectName" id="projectName"> <button class="createNewProject">Add New Project</button>`
+
         }
-        console.log(createProj.value)
+        document.querySelector('.createNewProject').addEventListener('click', function(){
+            const newProj = document.querySelector('#projectName').value
+            if(newProj === ""){
+                return
+            }else{
+                projectList.push(newProj)
+                console.log(projectList)
+
+            }
+
+        })
     }
+
 
     createProj.addEventListener('click', projectName)
 
