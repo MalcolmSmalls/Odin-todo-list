ccdff3263db2a9917e6c5ef84c05df6140f832da
MalcolmSmalls <smallsbeats@gmail.com>
2022-09-28 19:33:42 -0700

    Add project name to selector

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "./getTask.js"
 import { listDOM } from "./DOM.js"
 import { deleteTask } from "./deleteTask.js"
 import { createProject } from "./createProject.js"
+import "./projectPicker.js"
 
 listDOM()
 // getTask()
--- src/projectPicker.js
+++ src/projectPicker.js
@@ -3,4 +3,13 @@ import { defaultProject } from "./defaultProject.js"
 
 const projectList = [defaultProject]
 
+projectList.forEach((project,index) => {
+    const projectSelector = document.querySelector('#project')
+    const options = document.createElement('option')
+    options.value = project[index].project
+    options.textContent = project[index].project
+    projectSelector.appendChild(options)
+
+})
+
 export { projectList }
