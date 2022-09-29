75540568aa0338658f8e7991786671beb5b7e713
MalcolmSmalls <smallsbeats@gmail.com>
2022-09-28 21:42:53 -0700

    Project switches when selected in selector

--- src/createProject.js
+++ src/createProject.js
@@ -3,30 +3,32 @@ import { projectList } from './projectPicker.js'
 const createProject = (() => {
     const createProj = document.querySelector('#project')
     const projectArea = document.querySelector('.projectSelected')
-    const projectName = () => {
-        if(createProj.value="newProject"){
+    const projectName = (e) => {
+        if(e.target.value==="newProject"){
             projectArea.innerHTML = `<label for="projectName">Project Name:</label> <input type="text" name="projectName" id="projectName"> <button class="createNewProject">Add New Project</button>`
 
+            document.querySelector('.createNewProject').addEventListener('click', function(){
+                const newProj = document.querySelector('#projectName').value
+                if(newProj === ""){
+                    return
+                }else{
+                    const newOption = document.createElement('option')
+                    newOption.value = `${newProj}`
+                    newOption.textContent = `${newProj}`
+                    projectList.push(newProj)
+                    document.querySelector('#project').appendChild(newOption)
+                    console.log(projectList)
+    
+                }
+    
+            })
         }
-        document.querySelector('.createNewProject').addEventListener('click', function(){
-            const newProj = document.querySelector('#projectName').value
-            if(newProj === ""){
-                return
-            }else{
-                const newOption = document.createElement('option')
-                newOption.value = `${newProj}`
-                newOption.textContent = `${newProj}`
-                projectList.push(newProj)
-                document.querySelector('#project').appendChild(newOption)
-                console.log(projectList)
 
-            }
-
-        })
-    }
+        }
 
 
-    createProj.addEventListener('click', projectName)
+
+    createProj.addEventListener('change', projectName)
 
 })()
 
--- src/projectPicker.js
+++ src/projectPicker.js
@@ -2,14 +2,23 @@
 import { defaultProject } from "./defaultProject.js"
 
 const projectList = [defaultProject]
+let currentProj
 
 projectList.forEach((project,index) => {
     const projectSelector = document.querySelector('#project')
     const options = document.createElement('option')
     options.value = project[index].project
     options.textContent = project[index].project
+    options.className = project[index].project
     projectSelector.appendChild(options)
+    const pickProj = document.querySelector(`#project`)
+    const switchProj = (e) => {
+        currentProj = e.target.value
+        console.log(currentProj)
+    }
+    pickProj.addEventListener('change', switchProj)
 
 })
 
 export { projectList }
+export { currentProj }
